e36b934a42869d0c3adb72378fb4ad62e8d70159
kkuramitsu <kimio@konohascript.org>
2014-08-08 11:30:10 +0900

    modified PEGUtils

--- src/org/peg4d/PEGUtils.java
+++ src/org/peg4d/PEGUtils.java
@@ -1,51 +1,34 @@
 package org.peg4d;
 
 public class PEGUtils {
-	public final static long memoKey(long pos, Peg e) {
-		return (pos << 16) | e.semanticId;
+	
+	public final static long objectId(long pos, short pegid) {
+		return (pos << 16) | pegid;
 	}
+	
 	public final static long objectId(long pos, Peg e) {
 		return (pos << 16) | e.uniqueId;
+	}
+	
+	public final static long objectIdNull(long pos, Peg e) {
+		if(e == null) {
+			return objectId(pos, (short)0);
+		}
+		else {
+			return objectId(pos, e);
+		}
+	}
+	
+	public final static long memoKey(long pos, Peg e) {
+		return (pos << 16) | e.semanticId;
 	}
 	public final static long failure(long pos, Peg e) {
 		return (pos << 16) | e.uniqueId;
 	}
-	public final static long getpos(long oid) {
-		return (oid >> 16);
+	public final static long getpos(long pospeg) {
+		return (pospeg >> 16);
 	}
-	public final static short getpegid(long oid) {
-		return (short)oid;
+	public final static short getpegid(long pospeg) {
+		return (short)pospeg;
 	}
-	public final static boolean isFailure(int oid) {
-		return oid == 0;
-	}
-	
-//	String s0(long oid) {
-//		if(oid == 1) {
-//			return "toplevel";
-//		}
-//		long pos = PEGUtils.getpos(oid);
-//		if(PEGUtils.isFailure(oid)) {
-//			return this.source.formatErrorMessage("syntax error", pos, "");
-//		}
-//		else {
-//			Peg e = this.peg.getPeg(oid);
-//			return "object " + pos + " peg=" + e;
-//		}
-//	}
-//
-//	String S(long oid) {
-//		if(oid == 1) {
-//			return "toplevel";
-//		}
-//		long pos = PEGUtils.getpos(oid);
-//		if(PEGUtils.isFailure(oid)) {
-//			return this.source.formatErrorMessage("syntax error", pos, "failure:");
-//		}
-//		else {
-//			Peg e = this.peg.getPeg(oid);
-//			return "pego(" + pos + "," + e.pegid2 + ")";
-//		}
-//	}
-
 }
