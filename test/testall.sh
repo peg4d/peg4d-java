e33e82b7318d2d82b8c96302d80f4be408457175
kkuramitsu <kimio@konohascript.org>
2014-10-01 15:53:20 +0900

    updated test

--- test/testall.sh
+++ test/testall.sh
@@ -1,27 +1,46 @@
 #!/bin/bash
 
 OUT=/dev/null
+PASS="((PASS))"
+FAIL="((FAIL))"
+NL=""
+#PASS="\e[34m((PASS))"
+#FAIL="\e[91m((FAIL))"
+#NL="\e[0m"
 
 function test_failure {
 	java -ea -jar peg4d.jar -p $1 test/empty >> OUT
 	if [ $? -eq 0 ]; then
-        	echo "((FAIL)) $1"
+        	echo -e "$FAIL $1 $NL"
 	else
-        	echo "((PASS)) $1"
+        	echo -e "$PASS $1 $NL"
 	fi
 }
 
 function test_match {
 	java -ea -jar peg4d.jar -p $1 test/empty >> OUT
 	if [ $? -eq 0 ]; then
-		echo "((PASS)) $1"
+		echo -e "$PASS $1 $NL"
 	else
-		echo "((FAIL)) $1"
+		echo -e "$FAIL $1 $NL"
 	fi
 }
 
+function test_tag {
+	RESULT=$(java -ea -jar peg4d.jar -p $1 -t tag $2)
+	if [ $? -eq 0 -a {$RESULT} = {$3} ]; then
+		echo -e "$PASS $1 $RESULT $NL"
+	else
+		echo -e "$FAIL $1 $RESULT $NL"
+	fi
+}
+
+
+test_failure test/always_fail.p4d
 test_failure test/example.p4d
 test_failure test/bad-example.p4d
+test_failure test/leftrecursion.p4d
+test_failure test/indirectleftrecursion.p4d
 
 test_match test/peg.p4d
 test_match test/rule.p4d
@@ -32,3 +51,6 @@ test_match test/choice.p4d
 test_match test/flag.p4d
 test_match test/flag2.p4d
 
+test_match test/cyclic.p4d
+
+test_tag test/xml.p4d test/build.xml "#Attr:129#Element:88#Name:217#Text:3#Value:129"
