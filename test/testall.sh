83703b07e2ff98a87d025a2949dfdb2549df08fa
kkuramitsu <kimio@konohascript.org>
2014-10-01 16:00:01 +0900

    adding c test

--- test/testall.sh
+++ test/testall.sh
@@ -54,3 +54,7 @@ test_match test/flag2.p4d
 test_match test/cyclic.p4d
 
 test_tag test/xml.p4d test/build.xml "#Attr:129#Element:88#Name:217#Text:3#Value:129"
+
+test_tag test/c.p4d test/test.c "#Add:1#Address:2#Apply:186#ArrayName:10#Assign:100#Block:113#Break:9#Cast:4#Character:1#Declaration:69#Define:2#Div:1#Empty:1#Equals:45#ExpressionStatement:215#Field:46#Function:34#GreaterThan:11#If:65#Include:3#Integer:31#Key:19#KeyValue:19#LeftShift:1#LessThan:1#List:68#Minus:1#Name:792#Not:3#NotEquals:5#Param:79#PointerField:17#PrefixInc:1#Return:69#Source:1#Star:10#String:3#Sub:6#Switch:2#SwitchCase:4#SwitchDefault:2#TConst:9#TEnum:30#TInt:38#TName:13#TPointer:57#TStruct:17#TVoid:11#Value:19#VarDecl:68#While:6"
+
+
