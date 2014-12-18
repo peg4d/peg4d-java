6d6093ff79f4fdda0246939aca8a4bd740050816
Masahiro Ide <imasahiro9@gmail.com>
2014-12-18 12:39:40 +0900

    fix coding style

--- libpeg4d/p4d.c
+++ libpeg4d/p4d.c
@@ -1,10 +1,9 @@
 #include "parsing.h"
 
-ParsingLog P4D_newLog(ParsingContext ctx, MemoryPool pool)
-{
+ParsingLog P4D_newLog(ParsingContext ctx, MemoryPool pool) {
     ParsingLog l;
 #ifdef USE_MALLOC
-    l = (ParsingLog)malloc(sizeof (struct ParsingLog));
+    l = (ParsingLog)malloc(sizeof(struct ParsingLog));
 #else
     l = MemoryPool_AllocParsingLog(pool);
 #endif
@@ -13,21 +12,19 @@ ParsingLog P4D_newLog(ParsingContext ctx, MemoryPool pool)
     return l;
 }
 
-void P4D_unuseLog(ParsingContext ctx, ParsingLog log)
-{
+void P4D_unuseLog(ParsingContext ctx, ParsingLog log) {
     P4D_setObject(ctx, &log->childNode, NULL);
 #ifdef USE_MALLOC
     free(log);
 #endif
 }
 
-int P4D_markLogStack(ParsingContext ctx)
-{
+int P4D_markLogStack(ParsingContext ctx) {
     return ctx->logStackSize;
 }
 
-void P4D_lazyLink(ParsingContext ctx, ParsingObject parent, int index, ParsingObject child, MemoryPool pool)
-{
+void P4D_lazyLink(ParsingContext ctx, ParsingObject parent, int index,
+                  ParsingObject child, MemoryPool pool) {
     ParsingLog l = P4D_newLog(ctx, pool);
     P4D_setObject(ctx, &l->childNode, child);
     child->parent = parent;
@@ -37,8 +34,7 @@ void P4D_lazyLink(ParsingContext ctx, ParsingObject parent, int index, ParsingOb
     ctx->logStackSize += 1;
 }
 
-void P4D_lazyJoin(ParsingContext ctx, ParsingObject left, MemoryPool pool)
-{
+void P4D_lazyJoin(ParsingContext ctx, ParsingObject left, MemoryPool pool) {
     ParsingLog l = P4D_newLog(ctx, pool);
     P4D_setObject(ctx, &l->childNode, left);
     l->index = -9;
@@ -47,51 +43,51 @@ void P4D_lazyJoin(ParsingContext ctx, ParsingObject left, MemoryPool pool)
     ctx->logStackSize += 1;
 }
 
-void P4D_commitLog(ParsingContext ctx, int mark, ParsingObject newnode, MemoryPool pool)
-{
+void P4D_commitLog(ParsingContext ctx, int mark, ParsingObject newnode,
+                   MemoryPool pool) {
     ParsingLog first = NULL;
     int objectSize = 0;
-    while(mark < ctx->logStackSize) {
+    while (mark < ctx->logStackSize) {
         ParsingLog cur = ctx->logStack;
         ctx->logStack = ctx->logStack->next;
         ctx->logStackSize--;
-        if(cur->index == -9) { // lazyCommit
+        if (cur->index == -9) { // lazyCommit
             P4D_commitLog(ctx, mark, cur->childNode, pool);
             P4D_unuseLog(ctx, cur);
             break;
         }
-        if(cur->childNode->parent == newnode) {
+        if (cur->childNode->parent == newnode) {
             cur->next = first;
             first = cur;
             objectSize += 1;
-        }
-        else {
+        } else {
             P4D_unuseLog(ctx, cur);
         }
     }
-    if(objectSize > 0) {
-        newnode->child = (ParsingObject*)calloc(sizeof(ParsingObject), objectSize);
+    if (objectSize > 0) {
+        newnode->child =
+            (ParsingObject *)calloc(sizeof(ParsingObject), objectSize);
         newnode->child_size = objectSize;
-        for(int i = 0; i < objectSize; i++) {
+        for (int i = 0; i < objectSize; i++) {
             ParsingLog cur = first;
             first = first->next;
-            if(cur->index == -1) {
+            if (cur->index == -1) {
                 cur->index = i;
             }
             P4D_setObject(ctx, &newnode->child[cur->index], cur->childNode);
             P4D_unuseLog(ctx, cur);
         }
-        for(int i = 0; i < objectSize; i++) {
-            if(newnode->child[i] == NULL) {
-                P4D_setObject(ctx, &newnode->child[i], P4D_newObject(ctx, 0, pool));
+        for (int i = 0; i < objectSize; i++) {
+            if (newnode->child[i] == NULL) {
+                P4D_setObject(ctx, &newnode->child[i],
+                              P4D_newObject(ctx, 0, pool));
             }
         }
     }
 }
 
-void P4D_abortLog(ParsingContext ctx, int mark)
-{
-    while(mark < ctx->logStackSize) {
+void P4D_abortLog(ParsingContext ctx, int mark) {
+    while (mark < ctx->logStackSize) {
         ParsingLog l = ctx->logStack;
         ctx->logStack = ctx->logStack->next;
         ctx->logStackSize--;
@@ -99,27 +95,25 @@ void P4D_abortLog(ParsingContext ctx, int mark)
     }
 }
 
-ParsingObject P4D_newObject(ParsingContext ctx, long start, MemoryPool pool)
-{
+ParsingObject P4D_newObject(ParsingContext ctx, long start, MemoryPool pool) {
     ParsingObject o = MemoryPool_AllocParsingObject(pool);
-    o->refc       = 0;
-    o->oid        = 0;
-    o->start_pos  = start;
-    o->end_pos    = start;
-    o->tag        = "#empty";  // default
-    o->value      = NULL;
-    o->parent     = NULL;
-    o->child      = NULL;
+    o->refc = 0;
+    o->oid = 0;
+    o->start_pos = start;
+    o->end_pos = start;
+    o->tag = "#empty"; // default
+    o->value = NULL;
+    o->parent = NULL;
+    o->child = NULL;
     o->child_size = 0;
     return o;
 }
 
-void P4D_unusedObject(ParsingContext ctx, ParsingObject o)
-{
+void P4D_unusedObject(ParsingContext ctx, ParsingObject o) {
     o->parent = ctx->unusedObject;
     ctx->unusedObject = o;
-    if(o->child_size > 0) {
-        for(int i = 0; i < o->child_size; i++) {
+    if (o->child_size > 0) {
+        for (int i = 0; i < o->child_size; i++) {
             P4D_setObject(ctx, &(o->child[i]), NULL);
         }
         free(o->child);
@@ -127,11 +121,10 @@ void P4D_unusedObject(ParsingContext ctx, ParsingObject o)
     }
 }
 
-void P4D_setObject(ParsingContext ctx, ParsingObject *var, ParsingObject o)
-{
+void P4D_setObject(ParsingContext ctx, ParsingObject *var, ParsingObject o) {
     if (var[0] != NULL) {
         var[0]->refc -= 1;
-        if(var[0]->refc == 0) {
+        if (var[0]->refc == 0) {
             P4D_unusedObject(ctx, var[0]);
         }
     }
--- pegvm/pegvm.h
+++ pegvm/pegvm.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
+
 #ifndef PEGVM_H
 #define PEGVM_H
 #define PEGVM_DEBUG 0
@@ -23,92 +24,96 @@ typedef struct Instruction {
     struct Instruction *jump;
 } PegVMInstruction, Instruction;
 
-#define PEGVM_OP_EACH(OP)\
-    OP(EXIT)\
-    OP(JUMP)\
-    OP(CALL)\
-    OP(RET)\
-    OP(IFSUCC)\
-    OP(IFFAIL)\
-    OP(REPCOND)\
-    OP(BYTE)\
-    OP(STRING)\
-    OP(CHAR)\
-    OP(CHARSET)\
-    OP(ANY)\
-    OP(NOTBYTE)\
-    OP(NOTANY)\
-    OP(NOTCHARSET)\
-    OP(NOTBYTERANGE)\
-    OP(NOTSTRING)\
-    OP(ANDBYTE)\
-    OP(ANDCHARSET)\
-    OP(ANDBYTERANGE)\
-    OP(ANDSTRING)\
-    OP(OPTIONALBYTE)\
-    OP(OPTIONALCHARSET)\
-    OP(OPTIONALBYTERANGE)\
-    OP(OPTIONALSTRING)\
-    OP(ZEROMOREBYTERANGE)\
-    OP(ZEROMORECHARSET)\
-    OP(PUSHo)\
-    OP(PUSHconnect)\
-    OP(PUSHp)\
-    OP(PUSHf)\
-    OP(PUSHm)\
-    OP(POP)\
-    OP(POPo)\
-    OP(STOREo)\
-    OP(STOREp)\
-    OP(STOREf)\
-    OP(STOREm)\
-    OP(FAIL)\
-    OP(SUCC)\
-    OP(NEW)\
-    OP(NEWJOIN)\
-    OP(COMMIT)\
-    OP(ABORT)\
-    OP(LINK)\
-    OP(SETendp)\
-    OP(TAG)\
-    OP(VALUE)\
-    OP(MAPPEDCHOICE)\
-    OP(PUSHconnect_CALL)\
-    OP(PUSHp_CALL)\
-    OP(PUSHp_STRING)\
-    OP(PUSHp_CHAR)\
-    OP(PUSHp_ZEROMORECHARSET)\
-    OP(PUSHp_PUSHp)\
-    OP(POP_JUMP)\
-    OP(POP_REPCOND)\
-    OP(STOREo_JUMP)\
-    OP(STOREp_JUMP)\
-    OP(STOREp_ZEROMORECHARSET)\
-    OP(STOREp_PUSHp)\
-    OP(STOREp_POP)\
-    OP(STOREp_TAG)\
-    OP(FAIL_JUMP)\
-    OP(SUCC_STOREp)\
-    OP(NEW_BYTE)\
-    OP(NEW_STRING)\
-    OP(NEW_PUSHp)\
-    OP(COMMIT_JUMP)\
-    OP(ABORT_JUMP)\
-    OP(ABORT_STOREo)\
-    OP(SETendp_POP)\
-    OP(TAG_JUMP)\
+#define PEGVM_OP_EACH(OP)      \
+    OP(EXIT)                   \
+    OP(JUMP)                   \
+    OP(CALL)                   \
+    OP(RET)                    \
+    OP(IFSUCC)                 \
+    OP(IFFAIL)                 \
+    OP(REPCOND)                \
+    OP(BYTE)                   \
+    OP(STRING)                 \
+    OP(CHAR)                   \
+    OP(CHARSET)                \
+    OP(ANY)                    \
+    OP(NOTBYTE)                \
+    OP(NOTANY)                 \
+    OP(NOTCHARSET)             \
+    OP(NOTBYTERANGE)           \
+    OP(NOTSTRING)              \
+    OP(ANDBYTE)                \
+    OP(ANDCHARSET)             \
+    OP(ANDBYTERANGE)           \
+    OP(ANDSTRING)              \
+    OP(OPTIONALBYTE)           \
+    OP(OPTIONALCHARSET)        \
+    OP(OPTIONALBYTERANGE)      \
+    OP(OPTIONALSTRING)         \
+    OP(ZEROMOREBYTERANGE)      \
+    OP(ZEROMORECHARSET)        \
+    OP(PUSHo)                  \
+    OP(PUSHconnect)            \
+    OP(PUSHp)                  \
+    OP(PUSHf)                  \
+    OP(PUSHm)                  \
+    OP(POP)                    \
+    OP(POPo)                   \
+    OP(STOREo)                 \
+    OP(STOREp)                 \
+    OP(STOREf)                 \
+    OP(STOREm)                 \
+    OP(FAIL)                   \
+    OP(SUCC)                   \
+    OP(NEW)                    \
+    OP(NEWJOIN)                \
+    OP(COMMIT)                 \
+    OP(ABORT)                  \
+    OP(LINK)                   \
+    OP(SETendp)                \
+    OP(TAG)                    \
+    OP(VALUE)                  \
+    OP(MAPPEDCHOICE)           \
+    OP(PUSHconnect_CALL)       \
+    OP(PUSHp_CALL)             \
+    OP(PUSHp_STRING)           \
+    OP(PUSHp_CHAR)             \
+    OP(PUSHp_ZEROMORECHARSET)  \
+    OP(PUSHp_PUSHp)            \
+    OP(POP_JUMP)               \
+    OP(POP_REPCOND)            \
+    OP(STOREo_JUMP)            \
+    OP(STOREp_JUMP)            \
+    OP(STOREp_ZEROMORECHARSET) \
+    OP(STOREp_PUSHp)           \
+    OP(STOREp_POP)             \
+    OP(STOREp_TAG)             \
+    OP(FAIL_JUMP)              \
+    OP(SUCC_STOREp)            \
+    OP(NEW_BYTE)               \
+    OP(NEW_STRING)             \
+    OP(NEW_PUSHp)              \
+    OP(COMMIT_JUMP)            \
+    OP(ABORT_JUMP)             \
+    OP(ABORT_STOREo)           \
+    OP(SETendp_POP)            \
+    OP(TAG_JUMP)               \
     OP(TAG_SETendp)
-    //OP(DTABLE)
+// OP(DTABLE)
 
 enum pegvm_opcode {
 #define DEFINE_ENUM(NAME) PEGVM_OP_##NAME,
     PEGVM_OP_EACH(DEFINE_ENUM)
 #undef DEFINE_ENUM
-    PEGVM_OP_ERROR = -1
+        PEGVM_OP_ERROR = -1
 };
 
-PegVMInstruction *loadByteCodeFile(ParsingContext context, PegVMInstruction *inst, const char *fileName);
+PegVMInstruction *loadByteCodeFile(ParsingContext context,
+                                   PegVMInstruction *inst,
+                                   const char *fileName);
 int ParserContext_Execute(ParsingContext context, PegVMInstruction *inst);
-extern long PegVM_Execute(ParsingContext context, Instruction *inst, MemoryPool pool);
-extern Instruction* PegVM_Prepare(ParsingContext context, Instruction *inst, MemoryPool pool);
+extern long PegVM_Execute(ParsingContext context, Instruction *inst,
+                          MemoryPool pool);
+extern Instruction *PegVM_Prepare(ParsingContext context, Instruction *inst,
+                                  MemoryPool pool);
 #endif
