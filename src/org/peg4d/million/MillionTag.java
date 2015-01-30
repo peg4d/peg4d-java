eda66b4e598e561647df79322569839532d59126
kensuketamura <kensuke.tamura.183@gmail.com>
2015-01-30 19:44:06 +0900

    fix scope

--- src/org/peg4d/million/MillionTag.java
+++ src/org/peg4d/million/MillionTag.java
@@ -121,5 +121,5 @@ public class MillionTag {
 	public static final int TAG_TABLE_SETTER_APPLY = ParsingTag.tagId("TableSetterApply");
 	public static final int TAG_VARIADIC_PARAMETER = ParsingTag.tagId("VariadicParameter");
 	public static final int TAG_OBJECT = ParsingTag.tagId("Object");
-	
+	public static final int TAG_PROPERTY = ParsingTag.tagId("Property");
 }
