8216ff06f274db39461be0dbdc958fc6e069d081
shun-honda <hondashun@honda-no-MacBook-Pro.local>
2015-02-20 23:45:31 +0900

    update gen_nezvm_update.sh

--- gen_nezvm_bytecode.sh
+++ gen_nezvm_bytecode.sh
@@ -1,42 +1,43 @@
 #!/bin/sh
 PEGDIR=../benchNEZVM/peg
-PEGDIR=../benchNEZVM/bytecodeFast
+BYTECODE=../benchNEZVM/bytecodeFast
+DUMP=../benchNEZVM/dump_bytecodeFast
 
 ant clean
 ant
 
-java -jar nez-0.9.2.jar conv --pegvm 7 -p ../benchNEZVM/peg/json.p4d -o ../benchNEZVM/bytecodeFast/json.bin > ../benchNEZVM/dump_bytecodeFast/json_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 7 -p $PEGDIR/json.p4d -o $BYTECODE/json.bin > $DUMP/json_bytecode.txt
 echo "\ngenerate json.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 7 -p ../benchNEZVM/peg/xml.p4d -o ../benchNEZVM/bytecodeFast/xml.bin > ../benchNEZVM/dump_bytecodeFast/xml_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 7 -p $PEGDIR/xml.p4d -o $BYTECODE/xml.bin > $DUMP/xml_bytecode.txt
 echo "\ngenerate xml.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 7 -p ../benchNEZVM/peg/c99.p4d -o ../benchNEZVM/bytecodeFast/c99.bin > ../benchNEZVM/dump_bytecodeFast/c99_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 7 -p $PEGDIR/c99.p4d -o $BYTECODE/c99.bin > $DUMP/c99_bytecode.txt
 echo "\ngenerate c99.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 8 -p ../benchNEZVM/peg/json.p4d -o ../benchNEZVM/bytecodeFast/json_noobj.bin > ../benchNEZVM/dump_bytecodeFast/json_noObj_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 8 -p $PEGDIR/json.p4d -o $BYTECODE/json_noobj.bin > $DUMP/json_noObj_bytecode.txt
 echo "\ngenerate json.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 8 -p ../benchNEZVM/peg/xml.p4d -o ../benchNEZVM/bytecodeFast/xml_noobj.bin > ../benchNEZVM/dump_bytecodeFast/xml_noObj_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 8 -p $PEGDIR/xml.p4d -o $BYTECODE/xml_noobj.bin > $DUMP/xml_noObj_bytecode.txt
 echo "\ngenerate xml.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 8 -p ../benchNEZVM/peg/c99.p4d -o ../benchNEZVM/bytecodeFast/c99_noobj.bin > ../benchNEZVM/dump_bytecodeFast/c99_noObj_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 8 -p $PEGDIR/c99.p4d -o $BYTECODE/c99_noobj.bin > $DUMP/c99_noObj_bytecode.txt
 echo "\ngenerate c99.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 9 -p ../benchNEZVM/peg/json.p4d -o ../benchNEZVM/bytecodeFast/json_noInline.bin > ../benchNEZVM/dump_bytecodeFast/json_noInline_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 9 -p $PEGDIR/json.p4d -o $BYTECODE/json_noInline.bin > $DUMP/json_noInline_bytecode.txt
 echo "\ngenerate json.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 9 -p ../benchNEZVM/peg/xml.p4d -o ../benchNEZVM/bytecodeFast/xml_noInline.bin > ../benchNEZVM/dump_bytecodeFast/xml_noInline_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 9 -p $PEGDIR/xml.p4d -o $BYTECODE/xml_noInline.bin > $DUMP/xml_noInline_bytecode.txt
 echo "\ngenerate xml.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 9 -p ../benchNEZVM/peg/c99.p4d -o ../benchNEZVM/bytecodeFast/c99_noInline.bin > ../benchNEZVM/dump_bytecodeFast/c99_noInline_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 9 -p $PEGDIR/c99.p4d -o $BYTECODE/c99_noInline.bin > $DUMP/c99_noInline_bytecode.txt
 echo "\ngenerate c99.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 10 -p ../benchNEZVM/peg/json.p4d -o ../benchNEZVM/bytecodeFast/json_noobj_noInline.bin > ../benchNEZVM/dump_bytecodeFast/json_noObj_noInline_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 10 -p $PEGDIR/json.p4d -o $BYTECODE/json_noobj_noInline.bin > $DUMP/json_noObj_noInline_bytecode.txt
 echo "\ngenerate json.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 10 -p ../benchNEZVM/peg/xml.p4d -o ../benchNEZVM/bytecodeFast/xml_noobj_noInline.bin > ../benchNEZVM/dump_bytecodeFast/xml_noObj_noInline_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 10 -p $PEGDIR/xml.p4d -o $BYTECODE/xml_noobj_noInline.bin > $DUMP/xml_noObj_noInline_bytecode.txt
 echo "\ngenerate xml.bin\n"
 
-java -jar nez-0.9.2.jar conv --pegvm 10 -p ../benchNEZVM/peg/c99.p4d -o ../benchNEZVM/bytecodeFast/c99_noobj_noInline.bin > ../benchNEZVM/dump_bytecodeFast/c99_noObj_noInline_bytecode.txt
+java -jar nez-0.9.2.jar conv --pegvm 10 -p $PEGDIR/c99.p4d -o $BYTECODE/c99_noobj_noInline.bin > $DUMP/c99_noObj_noInline_bytecode.txt
 echo "\ngenerate c99.bin\n"
