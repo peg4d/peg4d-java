b0da6d431f9762a771d9df9cea477ec4ace4e89d
Masahiro Ide <imasahiro9@gmail.com>
2015-02-21 11:11:00 +0900

    fix memory_pool size

--- pegvm/memroy_pool.c
+++ pegvm/memroy_pool.c
@@ -8,10 +8,10 @@ struct MemoryObjectHeader {
 
 MemoryPool nez_CreateMemoryPool(MemoryPool mpool, size_t init_size) {
   mpool->init_size = init_size;
-  mpool->object_pool =
-      (ParsingObject)malloc(sizeof(struct ParsingObject) * init_size);
-  mpool->log_pool = (ParsingLog)malloc(sizeof(struct ParsingLog) * init_size);
+  mpool->object_pool = (ParsingObject)malloc(init_size);
+  mpool->log_pool = (ParsingLog)malloc(init_size);
   mpool->oidx = mpool->lidx = 0;
+  assert(mpool->object_pool != NULL);
   return mpool;
 }
 
